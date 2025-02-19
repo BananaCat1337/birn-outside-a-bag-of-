567a83c62ec366b1aac58ef93e8ed7a840dc5d32
BananaCat1337 <7862403@gmail.com>
2025-02-20 05:56:25 +0700

    vadim recoding this

--- js/music.js
+++ js/music.js
@@ -1,12 +1,13 @@
 
 document.getElementById('music-toggle').addEventListener('change', function() {
+    let music = document.getElementById('music');
     if (this.checked) {
-        music.play();
+      music.play();
     } else {
-        music.pause();
-        music.currentTime = 0;
+      music.pause();
+      music.currentTime = 0;
     }
-});
+  });
 
 function toggleVoice() {
     voiceEnabled = !voiceEnabled;
